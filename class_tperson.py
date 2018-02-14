c9a06717863209b0c847aa10dd0a4265cd317d55
Oleg Lyash <zoyter@users.noreply.github.com>
2018-02-14 18:50:26 +0300

    q2

--- class_tperson.py
+++ class_tperson.py
@@ -16,14 +16,30 @@ class TPerson():
         self.soft_skill = soft_skill
     
     def add_event(self,data):
-        pass
+        """
+        """
+        for i in data:
+            self.myevents.append(TEvent(i))
+            
+        #n = 0
+        #for i in person.myevents:
+            #n += 1
+            #print("---------------")
+            #print("Событие №%s"%(n))
+            #for j in i:
+                #print("\t %s"%(j))
+            #print("----------------")
     
     def __str__(self):
+        """
+        """
         print("-------------------")
         print("ФИО: %s %s %s"%(self.name1, self.name2, self.name3))
         print("Дата рождения: %s"%(self.data))
 
     def update_status(self):
+        """
+        """
         kriterii=[[[1,3,3,4],[5,7,7,10],[10,12,12,15],[10,12,12,15],[4,6,7,8]],
                   [[4,6,6,6],[7,15,15,15],[9,20,20,20],[18,25,25,25],[0,0,0,0]],
                   [[4,6,6,6],[5,10,10,10],[6,15,15,15],[7,20,20,20],[0,0,0,0]],
